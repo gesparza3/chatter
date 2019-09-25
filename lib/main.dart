b8721c649c59e4a24f335e839300fa5ea4d6b81a
gesparza3 <esparzagrant@gmail.com>
2019-09-25 16:35:14 -0700

    Fixed error with build

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Hello Worldß'),
+      home: MyHomePage(title: 'Hello World'),
     );
   }
 }
